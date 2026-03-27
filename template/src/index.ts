0aafc833262610612cd8f813b158deb592e3fba1
khacpv <khacpv@gmail.com>
2026-03-27 10:36:12 +0700

    update auth

--- template/src/index.ts
+++ template/src/index.ts
@@ -4,7 +4,7 @@ import { R2Explorer } from "r2-explorer";
 export default R2Explorer({
 	readonly: true,
 	basicAuth: {
-		username: "tubewise",
-		password: "tubewise@2026Secure",
+		username: "cloudflare",
+		password: "cloudflare",
 	},
 });
